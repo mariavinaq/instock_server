e70ed1d1a78f13aacf625e2ad0b583dd115a3649
himanidesai <de99hi@gmail.com>
2024-10-09 14:15:53 -0400

    Removing the Post/Insert query from inventories

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -53,55 +53,6 @@ export const getInventoryItemById = async (req, res) => {
   }
 };
 
-//POST a new item in the inventories table
-export const add = async (req, res) => {
-  const { warehouse_id, item_name, description, category, status, quantity } = req.body;
-
-  if (!warehouse_id || !item_name || !description || !category || !status || !quantity ) {
-    return res.status(400).json({
-      message: "Please provide missing data"
-    });
-  }
-  const warehouse = await knex("warehouses")
-    .where({id : warehouse_id})
-    .first();
-
-  if (!warehouse) {
-    return res.status(400).json({
-      message: "Invalid warehouse id"
-    })
-  }
-
-  if (isNaN(quantity) || Number(quantity) < 0){
-    return res.status(400).json({
-      message: "Quantity value must be a number"
-    })
-  }
-
-  try {
-    const result = await knex("inventories")
-      .insert({
-        id:null,
-        warehouse_id,
-        item_name,
-        description,
-        category,
-        status,
-        quantity
-      });
-    
-    const [ id ] = result;
-    const newRecord = await knex("inventories")
-      .where({ id })
-      .first()
-    
-    res.status(201).json(newRecord)
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to create new inventory item: ${error}`
-    });
-  }
-};
 
 //PUT/UPDATE existing item in the inventories table
 export const edit = async (req,res) => {
